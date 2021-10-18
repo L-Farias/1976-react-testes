e4191fca30917de329669b4594dd2b9d1dc9fee6
L-Farias <leo.9.bloch@gmail.com>
2021-10-18 16:54:29 -0300

    Parte final dos testes de component react

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import App, {calcularNovoSaldo} from "./App";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App, { calcularNovoSaldo } from "./App";
 
 describe("Componente principal", () => {
   describe("Quando eu abro o App do banco", () => {
@@ -22,16 +22,29 @@ describe("Componente principal", () => {
     });
   });
 
-  describe('Quando eu realizo um transação' , ()=>{
-      it('que é um saque, o valor vai diminuir', ()=>{
-        
-        const valores = {
-            tran: 'saque',
-            valor: 50
-        }
-        
-        const novoSaldo = calcularNovoSaldo(valores, 150)
-        expect(novoSaldo).toBe(100);     
-    })
-  })
+  describe("Quando eu realizo um transação", () => {
+    it("que é um saque, o valor vai diminuir", () => {
+      const valores = {
+        tran: "saque",
+        valor: 50,
+      };
+
+      const novoSaldo = calcularNovoSaldo(valores, 150);
+      expect(novoSaldo).toBe(100);
+    });
+    it("que é um saque, a transação deve ser realizada", () => {
+      const { getByText, getByTextId, getByLabelText } = render(<App />);
+      const saldo = screen.getByText('R$ 1000');
+      const transacao = screen.getByLabelText('Saque');
+      const valor = screen.getByTextId('valor');
+      const botaoTransacao = screen.getByText('Realizar operação');
+
+      expect(saldo.textContent).toBe('R$ 1000')
+      
+      fireEvent.click(transacao, {target: {value:'saque'}})
+      fireEvent.change(valor, {target: {value: 10 }})
+      fireEvent.click(botaoTransacao)
+      expect(saldo.textContent).toBe('R$ 990');
+    });
+  });
 });
